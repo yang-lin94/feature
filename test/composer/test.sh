2afe6e6bf3894e93120ea9386a72ed776723afc9
yang-lin94 <casongg@gmail.com>
2025-06-15 11:44:04 +0800

    refactor: remove PHP availability check from composer installation script

--- test/composer/test.sh
+++ test/composer/test.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# This test can be run with the following command:
-#    devcontainer features test \
-#               --features composer \
-#               --base-image mcr.microsoft.com/devcontainers/base:ubuntu .
-
 set -e
 
 # Optional: Import test library bundled with the devcontainer CLI
@@ -13,7 +8,6 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 check "composer is installed" composer --version
-check "composer is in /usr/local/bin" test -f /usr/local/bin/composer
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
