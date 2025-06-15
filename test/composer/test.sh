56ad6b9d536b3c6d47f90eafb1809ba51a1a2abf
yang-lin94 <casongg@gmail.com>
2025-06-15 10:45:06 +0800

    refactor: remove redundant composer project creation tests from test script

--- test/composer/test.sh
+++ test/composer/test.sh
@@ -15,15 +15,6 @@ source dev-container-features-test-lib
 check "composer is installed" composer --version
 check "composer is in /usr/local/bin" test -f /usr/local/bin/composer
 
-# Test that composer can create a new project (basic functionality test)
-check "composer can create a new project" composer create-project --prefer-dist --no-interaction --no-dev laravel/laravel-test test-project
-
-# Clean up test project
-rm -rf test-project
-
-# Test composer self-update (optional, but good to verify it works)
-check "composer can check for updates" composer self-update --dry-run
-
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
 reportResults
